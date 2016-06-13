0746fd202b931d57a1583878647c4997e37ff332
Sangpil Kim <spkim921@gmail.com>
2016-06-12 20:39:52 -0400

    space

--- bloomberg/getPersonHistory.py
+++ bloomberg/getPersonHistory.py
@@ -11,7 +11,7 @@ req = re.get("http://www.bloomberg.com/profiles/people/18811315-marlon-l-sanchez
 soup = bs4(req.text, 'html.parser')
 careerHistory        = soup.findAll("div", { "class" : "markets_module bio_career" })
 corporateInformation = soup.findAll("div", { "class" : "markets_module corporate_info" })
-memberShips           = soup.findAll("div", { "class" : "markets_module bio_membership" })
+memberShips          = soup.findAll("div", { "class" : "markets_module bio_membership" })
 
 #print(careerHistory)
 #print(corporateInformation)
