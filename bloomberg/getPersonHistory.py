6926ea6374b69cccafb9456d577fe88238b687d6
Sangpil Kim <1004forall@gmail.com>
2016-06-12 20:19:16 -0400

    added memberships parse on soup

--- bloomberg/getPersonHistory.py
+++ bloomberg/getPersonHistory.py
@@ -9,8 +9,11 @@ import os
 
 req = re.get("http://www.bloomberg.com/profiles/people/18811315-marlon-l-sanchez")
 soup = bs4(req.text, 'html.parser')
-careerHistory = soup.findAll("div", { "class" : "markets_module bio_career" })
+careerHistory        = soup.findAll("div", { "class" : "markets_module bio_career" })
 corporateInformation = soup.findAll("div", { "class" : "markets_module corporate_info" })
+memberShips           = soup.findAll("div", { "class" : "markets_module bio_membership" })
 
-print(careerHistory)
+#print(careerHistory)
+#print(corporateInformation)
+print(memberShips)
 
