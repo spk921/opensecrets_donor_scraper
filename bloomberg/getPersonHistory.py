99544e37f6c4e8fba0ca8bc2e83360e102d0e106
Sangpil Kim <1004forall@gmail.com>
2016-06-12 20:08:24 -0400

    getPersonalHistory example added

---
+++ bloomberg/getPersonHistory.py
@@ -0,0 +1,16 @@
+#Made by Sangpil Kim
+#May 24 2016
+
+from bs4 import BeautifulSoup as bs4
+import requests as re
+import json
+import csv
+import os
+
+req = re.get("http://www.bloomberg.com/profiles/people/18811315-marlon-l-sanchez")
+soup = bs4(req.text, 'html.parser')
+careerHistory = soup.findAll("div", { "class" : "markets_module bio_career" })
+corporateInformation = soup.findAll("div", { "class" : "markets_module corporate_info" })
+
+print(careerHistory)
+
