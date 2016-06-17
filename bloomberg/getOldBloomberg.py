75a9f5bed99d0757ebecd2fc623d3e45b252adb6
Sangpil Kim <1004forall@gmail.com>
2016-06-16 23:08:03 -0400

    update geting old bloomberg info

---
+++ bloomberg/getOldBloomberg.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import json
+from google import search
+import csv
+
+def searchGoogle(query,dic):
+    bloomberg = []
+    forbes    = []
+    # later do forbes as well
+    for url in search(query, stop=10):
+        print(url)
+        if 'bloomberg.com/research/stocks/private/person' in url:
+            bloomberg.append(url)
+        if 'bloomberg.com/research/stocks/people/person' in url:
+            bloomberg.append(url)
+        if 'forbes.com/lists' in url:
+            forbes.append(url)
+    dic['bloomberg'] = bloomberg
+    dic['forbes']    = forbes
+    return dic
+
+def scrapInfo(name):
+    dic = {}
+    dic['name'] = name
+    query = dic['name']+ ' bloomberg'
+    info = searchGoogle(query,dic)
+    return info
+
+def readCSV(fileName):
+    with open(fileName,'rU') as csvfile:
+        names = []
+        companies  = []
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            names.append(row['ceoname'])
+            companies.append(row['coname'])
+    return names, companies
+
+tuples = readCSV('ceoname.csv')
+# Unpacking tuples
+names , _ = tuples
+print(names)
+conj = []
+#Scrap info
+for name in names:
+    conj.append(scrapInfo(name))
+    print(conj[len(conj)-1])
+#Dump as json
+with open(dstJson, mode='w', encoding='utf-8') as f:
+    json.dump(conj,f)
