11e2c5f4a1997d2b7f77c54fb030bd310d084e0a
spk <spkim921@gmail.com>
2016-06-28 04:47:53 -0400

    update googleSelenium

---
+++ test/README.md
@@ -0,0 +1,3 @@
+###Using phantomjs
+
+https://realpython.com/blog/python/headless-selenium-testing-with-python-and-phantomjs/
--- test/googleSelenium.py
+++ test/googleSelenium.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 import urllib.parse as urlparse
 
 driver = webdriver.PhantomJS()
-driver.get("https://www.google.com/search?q=test")
-
+query = 'test'
+driver.get("https://www.google.com/search?q="+query)
+#div.g means <div class='g'> <div>
 results = driver.find_elements_by_css_selector('div.g')
 print(len(results))
 link = results[0].find_element_by_tag_name("a")
 href = link.get_attribute("href")
-
+print(href)
+print(type(href))
 print(urlparse.parse_qs(urlparse.urlparse(href).query)["q"])
